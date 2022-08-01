421b51d2ebdd3e60a3cbde919b5f7ea12e64cd4d
Vasanth-2000 <109895963+Vasanth-2000@users.noreply.github.com>
2022-08-01 13:58:04 +0530

    Add files via upload

---
+++ constructors.java
@@ -0,0 +1,11 @@
+package projects;
+
+	//default constructor
+	class constructors{
+		int bankno;
+		String bankname;
+
+	void display() {
+		System.out.println(bankno+" "+bankname);
+	}
+	}
---
+++ constructorsDemo.java
@@ -0,0 +1,15 @@
+package projects;
+
+	public class constructorsDemo {
+
+		public static void main(String[] args) {
+
+			constructors emp1=new constructors();
+		constructors emp2=new constructors ();
+
+			emp1.display();
+			emp2.display();
+			}
+
+
+}
