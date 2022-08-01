6c0a454c5cb6b5f044be15ac6f446b675403a421
Vasanth-2000 <109895963+Vasanth-2000@users.noreply.github.com>
2022-08-01 14:08:46 +0530

    Add files via upload

---
+++ Persons.java
@@ -0,0 +1,20 @@
+package projects;
+
+public class Persons {
+	int Personid;
+	String Personname;
+
+	Persons(int i,String n)
+	{
+	Personid=i;
+	Personname=n;
+	}
+
+	void display() {
+	System.out.println(Personid+" "+Personname);
+	}
+}
+
+
+
+
