6c5d1b2889da3fe835ce468a9ef8e5211d0adfac
Vasanth-2000 <109895963+Vasanth-2000@users.noreply.github.com>
2022-08-04 16:25:58 +0530

    Add files via upload

---
+++ Myclass1.java
@@ -0,0 +1,20 @@
+package projects;
+
+public class Myclass1 
+	{
+		public static void main(String args[]){
+	        int[] array = new int[4];
+	        try 
+	        {
+	            array[7] = 3;
+	        }
+	        catch (ArrayIndexOutOfBoundsException e) 
+	        {
+	            System.out.println("Array index is out of bounds!"); 
+	        }
+	        finally 
+	        {
+	            System.out.println("The array is of size " + array.length);
+	        }
+	    }
+	}
